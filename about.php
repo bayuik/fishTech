8eb88feaa7a47db408d025a0e980a1236f17fef3
bayuik <bayuindrakusuma05.com>
2021-06-28 10:07:05 +0700

    addd fish and update submit button in about

--- about.php
+++ about.php
@@ -1,4 +1,16 @@
 <?php require 'components/navbar.php' ?>
+<?php
+    if(isset($_POST["submit"])){
+        $nama = $_POST["txtName"];
+        $email = $_POST["txtEmail"];
+        $phone = $_POST["txtPhone"];
+        $textSaran = $_POST["txtMsg"];
+
+        $teks = "Nama saya: ".$nama . "%0AEmail: ".$email . "%0ANo Telp: " .$phone . "%0A" . $textSaran;
+        $wa = str_replace(" ", "%20", $teks);
+        header("Location: https://api.whatsapp.com/send?phone=6282244879410&text=$wa");
+    }
+?>
 
 <section id="about">
     <div class="container pt-5 mb-5">
@@ -28,27 +40,28 @@
 <section id="contact">
     <div class="container">
         <h3 class="pt-5 text-center mb-5">Drop Us a Message</h3>
-        <div class="row">
-            <div class="col-md-6">
-                <div class="form-group">
-                    <input type="text" name="txtName" class="form-control" placeholder="Your Name *" value="" />
+        <form action="about.php" method="POST">
+            <div class="row">
+                <div class="col-md-6">
+                    <div class="form-group">
+                        <input type="text" name="txtName" class="form-control" placeholder="Your Name *" value="" />
+                    </div>
+                    <div class="form-group">
+                        <input type="text" name="txtEmail" class="form-control" placeholder="Your Email *" value="" />
+                    </div>
+                    <div class="form-group">
+                        <input type="text" name="txtPhone" class="form-control" placeholder="Your Phone Number *"
+                            value="" />
+                    </div>
                 </div>
-                <div class="form-group">
-                    <input type="text" name="txtEmail" class="form-control" placeholder="Your Email *" value="" />
-                </div>
-                <div class="form-group">
-                    <input type="text" name="txtPhone" class="form-control" placeholder="Your Phone Number *"
-                        value="" />
+                <div class="col-md-6">
+                    <div class="form-group">
+                        <textarea name="txtMsg" class="form-control" placeholder="Your Message *"
+                            style="width: 100%; height: 150px;"></textarea>
+                    </div>
+                    <input type="submit" name="submit" class="btn btn-success mb-5 col-12 col-md-4 float-right" value="Send Your Message">
                 </div>
             </div>
-            <div class="col-md-6">
-                <div class="form-group">
-                    <textarea name="txtMsg" class="form-control" placeholder="Your Message *"
-                        style="width: 100%; height: 150px;"></textarea>
-                </div>
-                <a href="" class="btn btn-danger mb-5 col-12 col-md-4 float-right">Send Your Message</a>
-            </div>
-        </div>
         </form>
     </div>
 </section>
